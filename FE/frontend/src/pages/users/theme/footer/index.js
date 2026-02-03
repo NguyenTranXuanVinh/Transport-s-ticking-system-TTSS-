fb9a266b0a24b240b561a9c9416cfc8227e5c57c
doanbarin <doanbarin@gmail.com>
2026-02-03 20:45:20 +0700

    Update footer, header and global styles

--- FE/frontend/src/pages/users/theme/footer/index.js
+++ FE/frontend/src/pages/users/theme/footer/index.js
@@ -1,7 +1,75 @@
 import { memo } from "react";
-
+import { Link } from "react-router-dom";
+import "./style.scss";
+import {
+  BsFillTrainFrontFill,
+  BsTwitterX,
+  BsFacebook,
+  BsInstagram,
+} from "react-icons/bs";
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3">
+            <div className="footer__about">
+              <BsFillTrainFrontFill style={{ fontSize: "30px" }} />
+              <h1 className="footer__about_logo">TTTS</h1>
+              <ul>
+                <li>Địa Chỉ : Đà Nẵng</li>
+                <li>Phone:123456789</li>
+                <li>Email:Hikari@gmail.com</li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-lg-3">
+            <div className="footer__widget">
+              <h6>Cửa hàng</h6>
+              <ul>
+                <li>
+                  <Link to="">Liên hệ</Link>
+                </li>
+                <li>
+                  <Link to="">Thông tin về chúng tôi</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-lg-3">
+            <div className="footer__widget">
+              <h6>&nbsp;</h6>
+              {/*&nbsp;Non-Breaking Space, tạo khoảng trắng để cũng hàng với Cửa hàng  */}
+              <ul>
+                <li>
+                  <Link to="">Thông tin tài khoản</Link>
+                </li>
+                <li>
+                  <Link to="">Giỏ hàng</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-lg-3">
+            <div className="footer__widget">
+              <h6>Theo dõi chúng tôi</h6>
+              <ul>
+                <li>
+                  <BsFacebook /> Facebook
+                </li>
+                <li>
+                  <BsInstagram /> Instagram
+                </li>
+                <li>
+                  <BsTwitterX /> Twitter
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default memo(Footer);
--- FE/frontend/src/pages/users/theme/header/index.js
+++ FE/frontend/src/pages/users/theme/header/index.js
@@ -5,8 +5,13 @@ import {
   BsGithub,
   BsMailbox2Flag,
   BsPersonCircle,
+  BsFillTrainFrontFill,
+  BsCart4,
+  BsFillTicketPerforatedFill,
+  BsBusFrontFill,
 } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { formatter } from "utils/formatter";
 const Header = () => {
   return (
     <>
@@ -48,12 +53,49 @@ const Header = () => {
           </div>
         </div>
       </div>
-      <div className="container">
-        <div className="row">
-          <div className="col-xl-3">LOGO</div>
-          <div className="col-xl-6">MENU</div>
-          <div className="col-xl-3">PHONE</div>
+      <div className="header__bottom">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-4">
+              <div className="header_logo ">
+                <div style={{ fontSize: "30px" }}>
+                  <BsFillTrainFrontFill />
+                  <BsBusFrontFill />
+                  <span>Hệ thống bán vé tàu xe</span>
+                  <BsFillTicketPerforatedFill />
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-6">
+              <div className="header_cart">
+                <div
+                  className="header__cart_price"
+                  style={{ fontSize: "20px" }}
+                >
+                  <span>{formatter(300)}</span>
+                </div>
+                <ul>
+                  <li>
+                    <Link to="#">
+                      <BsCart4 />
+                      <span>5</span>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+      <div className="search_bar">
+        <form action="#">
+          <div>
+            <input type="text" placeholder="Tìm kiếm xe" />
+            <button type="submit" className="button-submit">
+              Tìm kiếm
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );
